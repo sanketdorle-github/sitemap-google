80c6ec26683acc1851abdbdb5860daf684aba947
sanketLocal <sanketgdorle200105@gmial.com>
2024-09-26 18:16:04 +0530

    commiting for render2

--- src/App.jsx
+++ src/App.jsx
@@ -1,2 +1,29 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Shop from "./pages/Shop";
+import Login from "./pages/Login";
+import Products from "./pages/Products";
+
+import { HelmetProvider } from "react-helmet-async";
+import Home from "./pages/Home";
+
+function App() {
+  return (
+    <>
+    <HelmetProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/shop" element={<Shop />} />
+         
+          <Route path="/login" element={<Login />} />
+          <Route path="/product" element={<Products />} />
+        </Routes>
+      </BrowserRouter>
+      
+      </HelmetProvider> 
+      </>
+  );
+}
+
 export default App;
