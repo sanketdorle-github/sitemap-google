2e4e1816e5936c046bdb31a51f493546a16d4fe5
sanketLocal <sanketgdorle200105@gmial.com>
2024-09-26 17:00:05 +0530

    final try with vite

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react()],build: {
+    outDir: 'dist', // Ensure the build files are outputted to the 'dist' directory
+  },
 })
