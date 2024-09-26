676c2ebdcfcd18c77dfafc3b4744450990552e51
sanketLocal <sanketgdorle200105@gmial.com>
2024-09-26 19:09:36 +0530

    removed base url code

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default {
-  base: '/',
-};
-
-// export default defineConfig({
-//   plugins: [react()],build: {
-//     outDir: 'dist', // Ensure the build files are outputted to the 'dist' directory
-//   },
-// })
+export default defineConfig({
+  plugins: [react()],
+  
+})
