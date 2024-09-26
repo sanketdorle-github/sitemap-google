9d103c56b9bceac7b021838ca440327dcfe7a116
sanketLocal <sanketgdorle200105@gmial.com>
2024-09-26 18:29:47 +0530

    commiting for render3

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],build: {
-    outDir: 'dist', // Ensure the build files are outputted to the 'dist' directory
-  },
-})
+export default {
+  base: '/',
+};
+
+// export default defineConfig({
+//   plugins: [react()],build: {
+//     outDir: 'dist', // Ensure the build files are outputted to the 'dist' directory
+//   },
+// })
